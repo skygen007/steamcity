9512d862f1048058315b4ebfa5694f2887fca77d
skygen007 <irichest1@gmail.com>
2015-11-21 00:15:11 +0200

    steam

--- db/migrate/20151120212509_create_users.rb
+++ db/migrate/20151120212509_create_users.rb
@@ -1,12 +1,12 @@
 class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
-      add_index :users, :uid, unique: true
       t.string :uid
       t.string :nickname
       t.string :avatar_url
 
       t.timestamps null: false
     end
+    add_index :users, :uid, unique: true
   end
 end
